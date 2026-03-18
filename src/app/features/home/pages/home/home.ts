1b9a4344201332c8c6e99b5290b93a2e8f18cd3c
Pedro Rama <pedro.ramag@gmail.com>
2026-03-18 11:13:08 +0100

    feat(Home): Logo de fondo y secciones

--- src/app/features/home/pages/home/home.ts
+++ src/app/features/home/pages/home/home.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { HomeAboutMyWeb } from '../../../aboutMyWeb/pages/home-about-my-web/home-about-my-web';
+import { HomeAplications } from '../../../aplications/pages/home-aplications/home-aplications';
+import { HomeExperience } from '../../../experience/pages/home-experience/home-experience';
+import { HomeProjects } from '../../../projects/pages/home-projects/home-projects';
+import { HomeStudies } from '../../../studies/pages/home-studies/home-studies';
+import { HomeUpdates } from '../../../updates/pages/home-updates/home-updates';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    RouterLink,
+    HomeAboutMyWeb, HomeAplications, HomeExperience, HomeProjects, HomeStudies, HomeUpdates
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home { }
